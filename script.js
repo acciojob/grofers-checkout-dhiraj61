fc8f174e83eac9adeac12ba10c4a8c178559d15c
Dhiraj Chaudhari <141726259+dhiraj61@users.noreply.github.com>
2026-06-12 10:13:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
+let priceList = Array.from(document.querySelectorAll('.price')).map(item => Number(item.innerText));
+let table = document.getElementsByTagName('table');
 
 const getSum = () => {
 //Add your code here
-  
+    let total = priceList.reduce((acc,curr)=> acc+curr);
+	let tr = document.createElement('tr');
+	let prtitleTd = document.createElement('td');
+	prtitleTd.innerText = 'Total Price';
+	tr.appendChild(prtitleTd);
+	let pricetd = document.createElement('td');
+	pricetd.innerText = total;
+	tr.appendChild(pricetd);
+	table[0].appendChild(tr);
 };
 
 getSumBtn.addEventListener("click", getSum);
